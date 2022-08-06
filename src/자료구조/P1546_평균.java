bbeb5251a234cc9135c7f247b5041fe5425757d4
jongchan988 <jongchan988@gmail.com>
2022-08-06 16:56:37 +0900

    feat: 시간복잡도 ~ 자료구조-구간합구하기 예제 추가
    - 시간복잡도
    - 숫자의합
    - 평균
    - 구간합구하기1
    - 구간합구하기2

---
+++ src/자료구조/P1546_평균.java
@@ -0,0 +1,27 @@
+package 자료구조;
+
+import java.util.Scanner;
+
+public class P1546_평균 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+
+        for(int i = 0; i < N; i++){
+            A[i] = sc.nextInt();
+        }
+
+        long sum = 0;
+        long max = 0;
+
+        for (int i = 0; i < N; i++){
+            if(A[i] > max) max = A[i];
+            sum = sum + A[i];
+        }
+        System.out.println("sum : "+ sum);
+        System.out.println("max : "+ max);
+        System.out.println("N : "+ N);
+        System.out.println(sum * 100 / max / N);
+    }
+}
