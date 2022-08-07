b5326d53fb0bf8e242d77389928d519cd63a62a0
jongchan988 <jongchan988@gmail.com>
2022-08-07 20:27:53 +0900

    feat: 자료구조-투 포인터(좋은수 - 주어진 N개의 수에서 다른 두수의 합으로 표현되는 수 구하기) 예제 추가

---
+++ src/자료구조/P1253_좋은수.java
@@ -0,0 +1,39 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P1253_좋은수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int Result = 0;
+        long A[] = new long[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++){
+            A[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        for (int k = 0; k < N; k++){
+            long find = A[k];
+            int i = 0;
+            int j = N-1;
+            while (i < j){
+                if(A[i] + A[j] == find){
+                    Result++;
+                    break;
+                } else if(A[i] + A[j] < find){
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        System.out.println(Result);
+        bf.close();
+    }
+}
